713a0bfce30687c36c48c9f079d9bb60d9f1942a
Deni0012 <deni.dg00@icloud.com>
2025-03-05 15:10:48 +0100

    feat: calcolo sconto e km

--- js/main.js
+++ js/main.js
@@ -1,5 +1,75 @@
+// Form 
 const form = document.getElementById("ticketForm");
 const pUserName = document.getElementById("user-name");
 const pUserKm = document.getElementById("user-km");
 const pUserAge = document.getElementById("user-age");
 const pPrice = document.getElementById("price");
+
+// km
+const priceKm = 0.21;
+let discount = 0;
+let discountValue = 0;
+let fullPrice;
+let finalPrice;
+
+
+// Calcolo dello sconto 
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputDistanceToTravel = document.getElementById("km").value;
+    const distanceToTravel = parseFloat(inputDistanceToTravel.replace(",", "."));
+    const inputUserAge = document.getElementById("age").value;
+    const userAge = parseInt(inputUserAge);
+
+    let checkedKm = (km) => {
+        if (!isNaN(km)) {
+            fullPrice = priceKm * km;
+            return fullPrice;
+        }
+        else if (km === 0) {
+            alert(`Il valore ${km} è pari a 0.`);
+        }
+        else {
+            console.log(`Il valore ${km} non è un numero.`);
+        }
+    };
+
+
+    let checkedAge = (userAge) => {
+        if (!isNaN(userAge)) {
+            if (userAge < 18) {
+                discount = 20;
+                return discount;
+            } else if (userAge >= 65) {
+                discount = 40;
+                return discount;
+            }
+        } else {
+            console.log(`Il valore ${inputUserAge} non è un numero.`);
+        }
+    };
+
+
+
+    const price = checkedKm(distanceToTravel);
+    const discountApplied = checkedAge(userAge);
+
+    if ((price && price > 0) &&
+        (discountApplied && discountApplied >= 0)) {
+
+        if (discount !== 0) {
+            discountValue = (price * discountApplied) / 100;
+        }
+        finalPrice = (price - discountValue).toFixed(2);
+
+        pUserName.textContent = "Denise Di Genni";
+        pUserKm.textContent = `${inputDistanceToTravel} Km`;
+        pUserAge.textContent = `${userAge} anni`;
+        pPrice.textContent = `${finalPrice}€`;
+
+
+        console.log(`Il prezzo finito per questo biglietto è di ${finalPrice}€`);
+    } else {
+        console.log("Impossibile calcolare il prezzo del biglietto.");
+    }
+})
