1bcf2d2f9ea3ad73bf31fa0a030ecc7af9a29fd9
Deni0012 <deni.dg00@icloud.com>
2025-03-05 15:05:04 +0100

    feat: creazione di form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log('Ciao Denise')
+const form = document.getElementById("ticketForm");
+const pUserName = document.getElementById("user-name");
+const pUserKm = document.getElementById("user-km");
+const pUserAge = document.getElementById("user-age");
+const pPrice = document.getElementById("price");
